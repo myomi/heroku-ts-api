a4278c2ed5a5f8d54ea3b119e6a01ec0a6c4b6ff
MITSUYA MYOUCHIN <m.myouchin@nakamura-joho.co.jp>
2019-05-25 17:08:32 +0900

    Added /api/login

--- index.ts
+++ index.ts
@@ -4,6 +4,20 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+app.post("/api/login", (req, res) => {
+  const loginRequest = JSON.parse(req.body) as LoginRequest;
+  if (loginRequest.username === 'user01' && loginRequest.password === '1234') {
+    return res.status(200);
+  } else {
+    return res.status(401);
+  }
+});
+
 app.get("/api/hello", (req, res) => {
   return res.json({
     message: "Hello, World"
