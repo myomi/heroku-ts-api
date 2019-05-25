d4249499cf189977e17d57fafe7246c3288dc3a5
MITSUYA MYOUCHIN <m.myouchin@nakamura-joho.co.jp>
2019-05-25 16:14:31 +0900

    Added ts-node to dependency

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import express from "express";
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import * as express from "express";
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
